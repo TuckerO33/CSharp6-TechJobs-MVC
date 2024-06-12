964c7eaf7cd8c8ce0a0d9d4605e778d574861d96
Tucker Overmann <tuckerovermann@gmail.com>
2024-06-12 14:46:40 -0500

    Passes autograder tests 2 and 3, actual page doesn't work tho, not sure how to make it work..

--- TechJobsMVCAutograded6/Controllers/ListController.cs
+++ TechJobsMVCAutograded6/Controllers/ListController.cs
@@ -44,6 +44,18 @@ public class ListController : Controller
     // TODO #2 - Complete the Jobs action method
     public IActionResult Jobs(string column, string value)
     {
+        List<Job> jobs = new();
+        if (column == "all")
+        {
+            jobs = JobData.FindAll();
+            ViewBag.title = "All Jobs";
+        }
+        else 
+        {
+            jobs = JobData.FindByColumnAndValue(column, value);
+            ViewBag.title = column + "Jobs";
+        }
+        ViewBag.jobs = jobs;
         return View();
     }
 }
--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -18,5 +18,20 @@ public class SearchController : Controller
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+        List<Job> jobs = [];
+        if (searchTerm == "all" || searchTerm == "")
+        {
+            jobs = JobData.FindAll();
+        }
+        else
+        {
+            jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+        }
+        ViewBag.jobs = jobs;
+        ViewBag.columns = ListController.ColumnChoices;
+        return View();
+    }
 }
 
